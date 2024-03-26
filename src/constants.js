d8cfeceaf546423d8e707350379371d584a10404
vidya-ramakrishnan <163241361+vidya-ramakrishnan@users.noreply.github.com>
2024-03-26 16:22:31 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 module.exports = {
-    BASE_URL: 'https://t42183.sandbox.mozu.com/api', // replace with your sandbox URL
+    BASE_URL: 'https://t<tenantId>.sandbox.mozu.com/api', // replace with your tenant id of your sandbox 
     ORDERS_URI: '/commerce/orders',
     SHIPMENTS_URI: '/commerce/shipments',
     ORDER_CANCEL_REASONS_URI: '/commerce/orders/cancel/reasons',
     ORDER_CANCEL_URI: '/commerce/orders/cancel',
     SET_FULFILLMENTINFO_URI: '/fulfillmentinfo',
     OAUTH_URI: '/platform/applications/authtickets/oauth',
-    KIBO_CLIENT_ID: 'irespons.Api_Proxy.1.0.0.Release',
-    KIBO_CLIENT_SECRET: '47a3afbde5cf4ee49e54daea64f5be12'
+    KIBO_CLIENT_ID: '<Client_Id>', // replace with your application client id created in your sandbox
+    KIBO_CLIENT_SECRET: '<Client_Secret>' // replace with your client secret for your application created at sandbox
 
   };
   
