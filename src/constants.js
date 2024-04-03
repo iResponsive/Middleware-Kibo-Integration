fba4d2d597d7d1522a15dfa9903429e1d721a57c
vidya-ramakrishnan <163241361+vidya-ramakrishnan@users.noreply.github.com>
2024-04-03 21:48:34 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 module.exports = {
-    BASE_URL: 'https://t<tenantId>.sandbox.mozu.com/api', // replace with your tenant id of your sandbox 
+    BASE_URL: 'https://t<tenantId>.sandbox.mozu.com/api', // replace with your tenant id of your kibo sandbox 
     ORDERS_URI: '/commerce/orders',
     SHIPMENTS_URI: '/commerce/shipments',
     ORDER_CANCEL_REASONS_URI: '/commerce/orders/cancel/reasons',
     ORDER_CANCEL_URI: '/commerce/orders/cancel',
     SET_FULFILLMENTINFO_URI: '/fulfillmentinfo',
     OAUTH_URI: '/platform/applications/authtickets/oauth',
-    KIBO_CLIENT_ID: '<Client_Id>', // replace with your application client id created in your sandbox
-    KIBO_CLIENT_SECRET: '<Client_Secret>' // replace with your client secret for your application created at sandbox
+    KIBO_CLIENT_ID: '<Client_Id>', // replace with your application client id created in your kibo sandbox
+    KIBO_CLIENT_SECRET: '<Client_Secret>' // replace with your client secret for your application created at kibo sandbox
 
   };
   
